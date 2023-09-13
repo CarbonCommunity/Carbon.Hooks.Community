44ebaac8e8e59b8dba5df3689e8195620b390a4b
raul <raulssorban@gmail.com>
2023-09-13 02:16:33 +0200

    Cleanup

--- src/Entity/WeaponRack/OnPickUpFromRack.cs
+++ src/Entity/WeaponRack/OnPickUpFromRack.cs
@@ -15,6 +15,7 @@ public partial class Category_Entity
 	{
 		[HookAttribute.Patch("OnPickupFromRack", "OnPickupFromRack", typeof(WeaponRack), "GivePlayerWeapon", new System.Type[] { typeof(BasePlayer), typeof(int), typeof(int), typeof(bool), typeof(bool) })]
 		[HookAttribute.Identifier("218d76c79e534841b0eacf66932f1769")]
+		[HookAttribute.Dependencies(new string[] { "CanPickupFromRack" })]
 
 		[MetadataAttribute.Info("Whenever a weapon was picked up from the rack.")]
 		[MetadataAttribute.Parameter("rack", typeof(WeaponRack))]
@@ -24,7 +25,7 @@ public partial class Category_Entity
 		[MetadataAttribute.Parameter("playerBeltIndex", typeof(int))]
 		[MetadataAttribute.Parameter("tryHold", typeof(bool))]
 
-		public class Entity_WeaponRack_218d76c79e534841b0eacf66932f1769 : Patch
+		public class Entity_WeaponRack_218d76c79e534841b0eacf66932f1769
 		{
 			// Implemented in CanPickupFromRack
 		}
