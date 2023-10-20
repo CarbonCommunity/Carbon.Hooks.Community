edf5aa75be09a67ba520045a712fe1176c23b9ce
raul <raulssorban@gmail.com>
2023-10-20 18:37:08 +0200

    Added vanilla bug fix patch - Place wall, place stocking, destroy wall - null HitInfo throws Rust error

---
+++ src/Patch/IOnKilledNREFix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Hooks;
+using UnityEngine;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Hooks;
+#pragma warning disable IDE0051
+
+public partial class Category_Fixes
+{
+	public partial class Fixes_OnKilledNREFix
+	{
+		[HookAttribute.Patch("IOnKilledNREFix", "IOnKilledNREFix", typeof(LootContainer), "OnKilled", new Type[] { typeof(HitInfo) })]
+		[HookAttribute.Identifier("adf38a97475361e166aa4f987d3e8985")]
+		[HookAttribute.Options(HookFlags.Hidden | HookFlags.Static | HookFlags.IgnoreChecksum)]
+
+		public class Fixes_OnKilledNREFix_adf38a97475361e166aa4f987d3e8985 : Patch
+		{
+			internal static HitInfo _reusableNulledHit = new();
+
+			public static void Prefix(ref HitInfo info)
+			{
+				if (info == null)
+				{
+					info = _reusableNulledHit ?? (_reusableNulledHit = new());
+				}
+			}
+		}
+	}
+}
