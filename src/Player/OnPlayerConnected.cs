6acd867227af61f1a8c11f0d72b39863f457652b
raul <raulssorban@gmail.com>
2024-03-10 15:31:00 +0100

    Added hook metadata

---
+++ src/Player/OnPlayerConnected.cs
@@ -0,0 +1,31 @@
+﻿using API.Hooks;
+using Carbon.Core;
+using Network;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Hooks;
+#pragma warning disable IDE0051
+
+public partial class Category_Player
+{
+	public partial class Player_Hooks
+	{
+		[HookAttribute.Patch("OnPlayerConnected", "OnPlayerConnected", typeof(CorePlugin), "IOnPlayerConnected")]
+		[HookAttribute.Options(HookFlags.MetadataOnly)]
+
+		[MetadataAttribute.Category("Player")]
+		[MetadataAttribute.Info("Called after the player object is created, but before the player has spawned.")]
+		[MetadataAttribute.Parameter("player", typeof(BasePlayer))]
+
+		public class OnPlayerConnected : Patch
+		{
+
+		}
+	}
+}
