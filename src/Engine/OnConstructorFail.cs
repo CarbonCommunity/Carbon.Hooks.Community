c1bf592246abd843ec101106b52368f9f1ac5fcd
raul <raulssorban@gmail.com>
2024-03-26 00:57:52 +0100

    Added `OnCompilationFail[150731668](string filePath, Trace[] errors, Trace[] warnings)` Added `OnConstructorFail[937285752](RustPlugin plugin, Exception exception)`

---
+++ src/Engine/OnConstructorFail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using API.Hooks;
+using Carbon.Core;
+using Carbon.Managers;
+using Oxide.Plugins;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Hooks;
+#pragma warning disable IDE0051
+
+public partial class Category_Engine
+{
+	public partial class Engine_Hooks
+	{
+		[HookAttribute.Patch("OnConstructorFail", "OnConstructorFail", typeof(ModLoader), nameof(ModLoader.InitializePlugin))]
+		[HookAttribute.Options(HookFlags.MetadataOnly)]
+
+		[MetadataAttribute.Category("Engine")]
+		[MetadataAttribute.Info("Gets called when a plugin is unloaded.")]
+		[MetadataAttribute.Parameter("plugin", typeof(RustPlugin))]
+		[MetadataAttribute.Parameter("exception", typeof(Exception))]
+		[MetadataAttribute.Assembly("Carbon.Common.dll")]
+
+		public class OnConstructorFail : Patch
+		{
+
+		}
+	}
+}
