bbb440240728a860259c5c751d14bc779c893ac5
raul <raulssorban@gmail.com>
2024-05-15 21:45:31 +0200

    Compile fix

--- src/Item/CanAcceptBackpackItem.cs
+++ src/Item/CanAcceptBackpackItem.cs
@@ -14,25 +14,5 @@ public partial class Category_Fun
 {
 	public partial class Fun_AnimalBrain
 	{
-		[HookAttribute.Patch("CanAcceptBackpackItem", "CanAcceptBackpackItem", typeof(ItemModBackpack), "CanAcceptItem", new System.Type[] { typeof(Item), typeof(Item), typeof(int) })]
-
-		[MetadataAttribute.Info("Gets called whenever attempting to place an item in a backpack item, overriding returning output.")]
-		[MetadataAttribute.Parameter("backpack", typeof(Item))]
-		[MetadataAttribute.Parameter("item", typeof(Item))]
-		[MetadataAttribute.Return(typeof(bool))]
-
-		public class CanAcceptBackpackItem : Patch
-		{
-			public static bool Prefix(Item backpack, Item item, int slot, ref bool __result)
-			{
-				if (HookCaller.CallStaticHook(2306141762, backpack, item) is bool hookValue)
-				{
-					__result = hookValue;
-					return false;
-				}
-
-				return true;
-			}
-		}
 	}
 }
