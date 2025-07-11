d38bba2b3d3c3f7ddf99db199fa3ff63f714ee4a
bubbafett5611 <metalgfx@gmail.com>
2025-07-11 01:29:37 +0100

    Added Blinding and Spectating hooks

---
+++ src/Player/OnCarbonBlinded.cs
@@ -0,0 +1,23 @@
+﻿using API.Hooks;
+using Carbon.Core;
+using Carbon.Modules;
+using Network;
+
+namespace Carbon.Hooks;
+
+#pragma warning disable IDE0051
+
+public partial class Category_Player
+{
+	public partial class Player_Hooks
+	{
+		[HookAttribute.Patch("OnCarbonBlinded", "OnCarbonBlinded", typeof(AdminModule), "BlindPlayer")]
+		[HookAttribute.Options(HookFlags.MetadataOnly)]
+
+		[MetadataAttribute.Category("Player")]
+		[MetadataAttribute.Info("Called when a player is blinded.")]
+		[MetadataAttribute.Parameter("player", typeof(BasePlayer))]
+
+		public class OnCarbonBlinded : Patch;
+	}
+}
